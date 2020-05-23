c7ffde9fc76da6b4ee58393a0d9d44c51d18fe04
Rollin Francois <rfrancois21@hotmail.com>
2020-05-23 02:29:57 -0400

    -UIPickerDelegate Methods -Get selected currency

--- ByteCoinApp/Controller/ViewController.swift
+++ ByteCoinApp/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -22,6 +22,8 @@ class ViewController: UIViewController, UIPickerViewDataSource {
         super.viewDidLoad()
 //         Set the ViewController class as the datasource to the currencyPicker object.
         currencyPicker.dataSource = self
+//         Set the ViewController class as the delegate for the currencyPicker object.
+        currencyPicker.delegate = self
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -31,6 +33,14 @@ class ViewController: UIViewController, UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
 //      Get the number of items in the array
         return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
     }
 }
 
