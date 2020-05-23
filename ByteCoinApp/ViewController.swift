decc093ec7ea062abc762304a0162682d2994feb
Rollin Francois <rfrancois21@hotmail.com>
2020-05-23 01:32:01 -0400

    Initial Commit

---
+++ ByteCoinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoinApp
+//
+//  Created by Rollin Francois on 5/23/20.
+//  Copyright © 2020 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
